3ff70bbf80dccefb5bb3bd35c3ef80ba0c929412
Hoang Mirs <hoang.mirs@gmail.com>
2018-10-01 09:39:27 +0700

    Init project

---
+++ PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,10 @@
+## What does this PR do ?
+
+- [ ] Implement function A
+- [ ] Fix bugs on function B
+
+## Details
+*(More details)*
+
+## Notes
+*(Other notes)*
---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module GoogleScraper
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.    
+
+    # Set the queuing backend to `Sidekiq`
+    # 
+    # Be sure to have the adapter's gem in your Gemfile
+    # and follow the adapter's specific installation
+    # and deployment instructions.
+    config.active_job.queue_adapter = :sidekiq
+
+    # Prefix the queue name of all jobs with Rails ENV
+    config.active_job.queue_name_prefix = Rails.env
+
+  end
+end
