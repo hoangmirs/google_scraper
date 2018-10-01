d595a297a0e90687c66c8ed3b498ad4b857df475
Hoang Mirs <hoang.mirs@gmail.com>
2018-10-01 14:35:48 +0700

    Add user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  class << self
+    def from_omniauth(auth_hash)
+      user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+      user.email = auth_hash['info']['email']
+      user.password = Devise.friendly_token[0,20]
+      user.save!
+      user
+    end
+  end
+end
